901020b36a0fee6de7c6ca474a323050a3a360b1
JohnPaulSteinborn <jsteinbor@ltu.edu>
2025-09-28 00:04:28 -0400

    Added end screen and restart

--- Assets/_Scripts/Ball.cs
+++ Assets/_Scripts/Ball.cs
@@ -4,19 +4,35 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    public AudioClip bounceSound;
+    public float speed = 36.0f;
     private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
+        transform.eulerAngles = new Vector3(0, 90, 0);
         rb = GetComponent<Rigidbody>();
 
-        rb.velocity = new Vector3(30.0f, 0f, 10.0f);
-    }
+        //create a random angle
+        float angle = Random.value * 44 - 22; //-22 to 22
+                                              // (Low - high) + low
+        //decide if it's going right or left
+        if (Random.value < .5f)
+        {
+            angle += 180;
+        }
+        //set the ball to that angle
+        transform.eulerAngles += new Vector3(0, angle, 0);
 
-    // Update is called once per frame
-    void Update()
+        //give it forward speed
+        rb.velocity = transform.forward * speed;
+    }
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if (!collision.transform.CompareTag("Floor"))
+        {
+            AudioSource.PlayClipAtPoint(bounceSound, transform.position, 1.0f);
+        }
     }
 }
--- Assets/_Scripts/DeathZone.cs
+++ Assets/_Scripts/DeathZone.cs
@@ -8,18 +8,26 @@ public class DeathZone : MonoBehaviour
     public Transform startPos;
     public GameObject ballPrefab;
     public int opposingPlayer;
+    public AudioClip outSound;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.CompareTag("Ball"))
         {
+            AudioSource.PlayClipAtPoint(outSound, other.transform.position, 1.0f);
             // Debug.Log($"{other.transform.name} just entered {transform.name}");
             // TODO: set other player's score up by 1
             GameManager.AddScore(opposingPlayer);
+
             // TODO: destroy this ball (other)
             Destroy(other.gameObject);
 
             // TODO: instantiate a new ball in the center
-            Instantiate(ballPrefab, startPos.position, Quaternion.identity);
+            if (GameManager.playing)
+            {
+                Instantiate(ballPrefab, startPos.position, Quaternion.identity);
+            }
+
         }
     }
 }
