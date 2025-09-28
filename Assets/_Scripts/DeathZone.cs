f4272f0311f42a676ba15be61d1f39a586cf1953
JohnPaulSteinborn <jsteinbor@ltu.edu>
2025-09-27 22:21:55 -0400

    added physics, controls, various scripts

---
+++ Assets/_Scripts/DeathZone.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Content;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+    public Transform startPos;
+    public GameObject ballPrefab;
+    public int opposingPlayer;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("Ball"))
+        {
+            // Debug.Log($"{other.transform.name} just entered {transform.name}");
+            // TODO: set other player's score up by 1
+            GameManager.AddScore(opposingPlayer);
+            // TODO: destroy this ball (other)
+            Destroy(other.gameObject);
+
+            // TODO: instantiate a new ball in the center
+            Instantiate(ballPrefab, startPos.position, Quaternion.identity);
+        }
+    }
+}
